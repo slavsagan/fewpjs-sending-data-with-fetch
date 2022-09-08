d4fed6aa592951d125f32d7a0fa30698bae93f3f
Slav <itiksolution@gmail.com>
2022-09-08 23:52:22 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your code here
+
+// let submitData = {
+//   name: "Slav",
+//   email: "student@gmail.com"
+// };
+
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(submitData)
+// };
+
+// fetch("http://localhost:3000/users", configObj)
+// .then(function(response) {       /** handling request */
+// return response.json()         /** recieve data we sent from the server */
+// })
+// .then(function(object) {         /** retrive this data */
+// console.log(object);
+// })
+// .catch(function(error) {        /** catching erros */
+// alert("Bad things, Slav!");
+// console.log(error.message);
+// });
+
+
+function submitData(name, email) {
+
+  fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'Steve',
+      email: 'steve@steve.com',
+    }),
+  })
+
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (object) {
+      getId(object)
+    })
+    .catch(function (error) {
+      alert('Bad things, Slav!')
+      console.log(error.message)
+    })
+}
